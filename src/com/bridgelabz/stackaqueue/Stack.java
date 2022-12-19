898cc55f6f473f74910bc788cdc4b7fda8026352
shiwani <shiwanipaunikar1@gmail.com>
2022-12-19 14:41:12 -0800

    [shiwani]code to create simple stack program is added

---
+++ src/com/bridgelabz/stackaqueue/Stack.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.stackaqueue;
+
+public class Stack<T>
+{
+    StackNode<T> head;
+
+    /**
+     * Method to add element to the stack
+     */
+    public void push(T data)
+    {
+        StackNode<T> newNode = new StackNode<>();
+        newNode.data = data;
+        newNode.next = null;
+        newNode.next = head;
+        head = newNode;
+    }
+
+    /**
+     * Method to print stack
+     */
+    public void printStack()
+    {
+        StackNode<T> tempNode = head;
+        while (tempNode != null)
+        {
+            System.out.println(tempNode.data);
+            tempNode = tempNode.next;
+        }
+    }
+
+}
