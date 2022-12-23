df86868aba487cd5cc8b08293f2843d015402ddb
shiwani <shiwanipaunikar1@gmail.com>
2022-12-23 11:17:08 -0800

    [shiwani]peek and pop program is added

--- src/com/bridgelabz/stackaqueue/StackNode.java
+++ src/com/bridgelabz/stackaqueue/StackNode.java
@@ -1,34 +1,9 @@
 package com.bridgelabz.stackaqueue;
 
-public class StackNode<T>
-{
+public class StackNode<T> {
     T data;
     StackNode<T> next;
 
-    //generating default constructor
-    public StackNode()
-    {
-        super();
+    public StackNode() {
     }
-
-    //generating getters and setters
-    public T getData()
-    {
-        return data;
-    }
-    public void setData(T data)
-    {
-        this.data = data;
-    }
-    public StackNode<T> getNext()
-    {
-        return next;
-    }
-    public void setNext(StackNode<T> next)
-    {
-        this.next = next;
-    }
-
-
 }
-
